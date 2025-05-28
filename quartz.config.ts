d464f0885ff88af010536d03ed48caca6cbc27fe
Michael Walton <walton.mike@proton.me>
2025-05-28 05:57:28 -0700

    Quartz sync: May 28, 2025, 5:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           //light: "#faf8f8",
-          light: "#fcf5e5",
+          light: "#ffffff",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
