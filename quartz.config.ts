c15229d0825f3a0b5430d4d0faeeabd2bb2974bf
Michael Walton <walton.mike@proton.me>
2025-05-28 22:34:05 -0700

    Quartz sync: May 28, 2025, 10:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "myke walton",
+    pageTitle: "myke | walton",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
